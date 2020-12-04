5a57489af5b2a4d801ecd3b9229d0cca001bc614
Stephan Schneider <stefichjo@yahoo.de>
2020-12-05 00:31:26 +0100

    filesystem

---
+++ src/FileSystem.hs
@@ -0,0 +1,23 @@
+module FileSystem where
+
+import System.IO (IOMode(..), hGetContents, hPrint, openFile, withFile)
+
+getFile :: Read a => FilePath -> IO a
+getFile = (read <$>) . readFileContents
+
+putFile :: Show a => FilePath -> a -> IO ()
+putFile = writeFileContents WriteMode
+
+addFile :: Show a => FilePath -> a -> IO ()
+addFile = writeFileContents AppendMode
+
+initFile :: FilePath -> IO ()
+initFile file = putFile file ""
+
+directory = "out/" :: FilePath
+
+readFileContents :: FilePath -> IO String
+readFileContents file = openFile (directory ++ file) ReadMode >>= hGetContents
+
+writeFileContents :: Show a => IOMode -> FilePath -> a -> IO ()
+writeFileContents mode file = withFile (directory ++ file) mode . flip hPrint
