334beb1fdab42e62bde05b177344973207ca8729
Stephan Schneider <stefichjo@yahoo.de>
2020-12-05 14:44:45 +0100

    ad hoc test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,12 @@
 module Main where
 
+import Test.Hspec
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "my ad-hoc test" $ do
+    it "should confirm that 2 + 2 == 4" $ do
+      2 + 2 `shouldBe` 4
