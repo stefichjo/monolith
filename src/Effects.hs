7266019c7e65b130d22f1a56edcb31cb7bfce836
Stephan Schneider <stefichjo@yahoo.de>
2020-12-07 21:23:49 +0100

    more renaming

--- src/Effects.hs
+++ src/Effects.hs
@@ -29,37 +29,82 @@ data User =
   deriving (
     Eq, Ord, Show, Read)
 
+-- TODO move to submodules Log'/Log
+
 class Log' m where
 
-  logWrite :: String -> m ()
+  logWrite' :: String -> m ()
 instance Log' IO where
 
-  logWrite = addFile logFileName
+  logWrite' = addFile logFileName
 class Console' m where
 
-  consoleRead :: m String
-  consoleWrite :: String -> m ()
+  consoleRead' :: m String
+  consoleWrite' :: String -> m ()
 instance Console' IO where
 
-  consoleRead = getLine
-  consoleWrite = putStrLn
+  consoleRead' = getLine
+  consoleWrite' = putStrLn
 class DB' m where
 
-  dbCreate :: User -> m ()
-  dbRead :: m [User]
+  dbCreate' :: User -> m ()
+  dbRead' :: m [User]
 
-  dbCreateNextUser :: Monad m => String -> m ()
-  dbCreateNextUser name = do
-    User lastId _ <- maximum <$> dbRead
-    dbCreate (User (succ lastId) name)
+  dbCreateNextUser' :: Monad m => String -> m ()
+  dbCreateNextUser' name = do
+    User lastId _ <- maximum <$> dbRead'
+    dbCreate' (User (succ lastId) name)
 instance DB' IO where
 
-  dbCreate = addFile dbFileName
-  dbRead = map read . lines <$> readFileContents dbFileName
+  dbCreate' = addFile dbFileName
+  dbRead' = map read . lines <$> readFileContents dbFileName
 
 type App' m a = (Monad m, Log' m, Console' m, DB' m) => m a
 
-app :: App' m ()
+app' :: App' m ()
+app' = do
+  consoleWrite' "Yes?"
+  name <- consoleRead'
+  logWrite' $ "New user: " <> name <> "."
+  dbCreateNextUser' name
+  consoleWrite' "Bye!"
+
+data Log m a where {
+
+    LogWrite :: String -> Log m ();
+  
+  }; makeSem ''Log
+withLogIO :: WithIO Log r
+withLogIO = interpret $ \case
+  LogWrite msg -> embed (addFile logFileName msg)
+data Console m a where {
+
+    ConsoleWrite :: String -> Console m ();
+    ConsoleRead :: Console m String;
+
+  }; makeSem ''Console
+withConsoleIO :: WithIO Console r
+withConsoleIO = interpret $ \case
+  ConsoleWrite line -> embed (putStrLn line)
+  ConsoleRead       -> embed getLine
+data DB m a where {
+
+    DbCreate :: User -> DB m ();
+    DbRead :: DB m [User];
+
+  }; makeSem ''DB
+withDbIO :: WithIO DB r
+withDbIO = interpret $ \case
+  DbCreate user -> embed . addFile dbFileName $ user
+  DbRead -> embed (map read . lines <$> readFileContents dbFileName)
+dbCreateNextUser :: String -> App r ()
+dbCreateNextUser name = do
+  User lastId  _ <- maximum <$> dbRead
+  dbCreate (User (succ lastId) name)
+
+type App r a = '[Console, DB, Log] `Members` r => Builder r a
+
+app :: App r ()
 app = do
   consoleWrite "Yes?"
   name <- consoleRead
@@ -67,51 +112,8 @@ app = do
   dbCreateNextUser name
   consoleWrite "Bye!"
 
-data Log m a where {
-
-    LogWriteDsl :: String -> Log m ();
-  
-  }; makeSem ''Log
-withLogIO :: WithIO Log r
-withLogIO = interpret $ \case
-  LogWriteDsl msg -> embed (addFile logFileName msg)
-data Console m a where {
-
-    ConsoleWriteDsl :: String -> Console m ();
-    ConsoleReadDsl :: Console m String;
-
-  }; makeSem ''Console
-withConsoleIO :: WithIO Console r
-withConsoleIO = interpret $ \case
-  ConsoleWriteDsl line -> embed (putStrLn line)
-  ConsoleReadDsl       -> embed getLine
-data DB m a where {
-
-    DbCreateDsl :: User -> DB m ();
-    DbReadDsl :: DB m [User];
-
-  }; makeSem ''DB
-withDbIO :: WithIO DB r
-withDbIO = interpret $ \case
-  DbCreateDsl user -> embed . addFile dbFileName $ user
-  DbReadDsl -> embed (map read . lines <$> readFileContents dbFileName)
-dbCreateNextUserDsl :: String -> App r ()
-dbCreateNextUserDsl name = do
-  User lastId  _ <- maximum <$> dbReadDsl
-  dbCreateDsl (User (succ lastId) name)
-
-type App r a = '[Console, DB, Log] `Members` r => Builder r a
-
-appDsl :: App r ()
-appDsl = do
-  consoleWriteDsl "Yes?"
-  name <- consoleReadDsl
-  logWriteDsl $ "New user: " <> name <> "."
-  dbCreateNextUserDsl name
-  consoleWriteDsl "Bye!"
-
 appIO :: IO ()
-appIO = appDsl
+appIO = app
   & withLogIO
   & withConsoleIO
   & withDbIO
@@ -119,18 +121,18 @@ appIO = appDsl
 
 withLog :: With Log r
 withLog = interpret $ \case
-  LogWriteDsl msg -> pure ()
+  LogWrite msg -> pure ()
 withConsole :: With Console r
 withConsole = interpret $ \case
-  ConsoleWriteDsl line -> pure ()
-  ConsoleReadDsl -> pure consoleConst
+  ConsoleWrite line -> pure ()
+  ConsoleRead -> pure consoleConst
 withDb :: With DB r
 withDb = interpret $ \case
-  DbCreateDsl user -> pure ()
-  DbReadDsl -> pure inMemoryDB
+  DbCreate user -> pure ()
+  DbRead -> pure inMemoryDB
 
 appConst :: Monad m => m ()
-appConst = appDsl
+appConst = app
   & withLog
   & withConsole
   & withDb
