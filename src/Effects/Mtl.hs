89f8cb5b20ec42721a478a121f21161fa344a255
Stephan Schneider <stefichjo@yahoo.de>
2020-12-10 19:42:58 +0100

    cleaning up

--- src/Effects/Mtl.hs
+++ src/Effects/Mtl.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE RankNTypes, TypeSynonymInstances, ConstrainedClassMethods #-}
+{-# LANGUAGE FlexibleInstances, GeneralizedNewtypeDeriving #-}
+
 module Effects.Mtl where
 
 import FileSystem
@@ -64,3 +66,73 @@ mainMock' = app'
 mainIO' :: IO ()
 mainIO' = app' >>= print
 
+
+
+type LogT = Writer String
+instance Log' LogT where
+
+  logWrite' = tell
+newtype LogMtl a =
+  
+  LogMtl {
+    runLogMtl :: LogT a }
+  deriving (
+    Functor, Applicative, Monad,
+    MonadWriter String)
+instance Log' LogMtl where
+
+  logWrite' msg = LogMtl $ logWrite' msg
+
+type ConsoleT = WriterT String (Reader String)
+instance Console' ConsoleT where
+
+  consoleRead' = ask
+  consoleWrite' = tell
+newtype ConsoleMtl a =
+
+  ConsoleMtl {
+    runConsoleMtl :: ConsoleT a }
+  deriving (
+    Functor, Applicative, Monad,
+    MonadReader String, MonadWriter String)
+instance Console' ConsoleMtl where
+
+  consoleRead' = ConsoleMtl $ consoleRead'
+  consoleWrite' msg = ConsoleMtl $ consoleWrite' msg
+
+type DBT = State [User]
+instance DB' DBT where
+
+  dbCreate' user = dbRead' >>= put . append user
+  dbRead' = get
+newtype DBMtl a =
+  
+  DBMtl {
+    runDBMtl :: DBT a }
+  deriving (
+    Functor, Applicative, Monad,
+    MonadState [User])
+instance DB' DBMtl where
+
+  dbCreate' user = DBMtl $ dbCreate' user
+  dbRead' = DBMtl $ get
+
+type AppT = StateT [User] ConsoleT
+newtype AppMtl a =
+
+  AppMtl {
+    runAppMtl :: AppT a }
+  deriving (
+    Functor, Applicative, Monad,
+    MonadReader String, MonadWriter String, MonadState [User])
+instance Log' AppMtl where
+
+  logWrite' = tell
+instance DB' AppMtl where
+
+  dbCreate' user = dbRead' >>= put . append user
+  dbRead' = get
+instance Console' AppMtl where
+
+  consoleRead' = ask
+  consoleWrite' = tell
