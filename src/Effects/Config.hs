977ae8d511eb5ec630089c92bd9cf8772db13038
Stephan Schneider <stefichjo@yahoo.de>
2020-12-12 20:04:15 +0100

    clean up-chen

--- src/Effects/Config.hs
+++ src/Effects/Config.hs
@@ -2,13 +2,3 @@ module Effects.Config where
 
 logFileName = "log" :: FilePath
 dbFileName = "db" :: FilePath
-
--- type Builder r = Sem r
--- type With dsl r = forall a. Builder (dsl ': r) a -> Builder r a
--- type WithIO r = Member (Embed IO) r
--- type Build m a = Monad m => Builder '[Embed m] a -> m a
--- build :: Monad m => Sem '[Embed m] a -> m a
--- build = runM
-
--- mainIO :: IO ()
--- mainIO = app >>= print
--- src/Effects/Sem.hs
+++ src/Effects/Sem.hs
@@ -62,3 +62,10 @@ runIO =
         ConsoleRead       -> embed getLine)
 mainIO :: IO ()
 mainIO = runIO app >>= print
+
+-- type Builder r = Sem r
+-- type With dsl r = forall a. Builder (dsl ': r) a -> Builder r a
+-- type WithIO r = Member (Embed IO) r
+-- type Build m a = Monad m => Builder '[Embed m] a -> m a
+-- build :: Monad m => Sem '[Embed m] a -> m a
+-- build = runM
