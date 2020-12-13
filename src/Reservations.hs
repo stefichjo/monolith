11726f80a647c1640c846411641a628fe6010f26
Stephan Schneider <stefichjo@yahoo.de>
2020-12-13 22:00:14 +0100

    parse Day

--- src/Reservations.hs
+++ src/Reservations.hs
@@ -4,6 +4,8 @@ module Reservations where
 import Data.Time.Calendar ( Day )
 import Numeric.Natural ( Natural )
 import GHC.Generics ( Generic )
+import Data.Map (fromList,  Map )
+import Data.Time.Format (parseTimeOrError, readTime, defaultTimeLocale)
 
 -- A_MODEL ("The Domain layer")
 
@@ -18,4 +20,25 @@ data Reservation = Reservation
 
 reservation :: Reservation
 reservation = Reservation {name = "Mr. Miller", quantity = 2, date = read "2020-06-01", email = "manfred@miller.com"}
+
+-- | a key value map holding a list of reservations for any given day
+type ReservationMap = Map Day [Reservation]
+
+-- >>> parseDay "2010-10-11"
+-- 2010-10-11
+
+parseDay :: String -> Day
+parseDay = parseTimeOrError True defaultTimeLocale "%Y-%m-%d"
+
+foo :: ReservationMap
+foo = fromList
+  [
+    (
+      parseDay "2020-06-01",
+        [
+          Reservation {date = parseDay "2020-06-01", name = "Mr. Miller", email = "manfred@miller.com", quantity = 2}, 
+          Reservation {date = parseDay "2020-06-01", name = "Andrew M. Jones", email = "amjones@example.com", quantity = 4}
+        ]
+    )
+  ]
 
