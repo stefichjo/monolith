ba7cab58225ac854e1b374320a6c0061bdc82156
Stephan Schneider <stefichjo@yahoo.de>
2021-01-24 19:43:46 +0100

    formatting

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Network.Wai
-import Network.HTTP.Types
+import Network.Wai ( responseLBS, Application )
+import Network.HTTP.Types ( status200 )
 import Network.Wai.Handler.Warp (run)
 
 import qualified Effects
@@ -10,17 +10,16 @@ import qualified Effects
 main' :: IO ()
 main' = Effects.main
 
-
-
 app :: Application
 app _ respond = do
-    putStrLn "I've done some IO here"
-    respond $ responseLBS
-        status200
-        [("Content-Type", "text/plain")]
-        "Hello, Web!"
+  putStrLn "I've done some IO here"
+  respond $
+    responseLBS
+      status200
+      [("Content-Type", "text/plain")]
+      "Hello, Web!"
 
 main :: IO ()
 main = do
-    putStrLn $ "http://localhost:8080/"
-    run 8080 app
+  putStrLn "http://localhost:8080/"
+  run 8080 app
